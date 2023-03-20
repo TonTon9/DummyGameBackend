9698fa24dd2448541952763d8b8a72d9aa8cd921
MaximShadrin <maxim@fancystudios.com>
2023-03-21 02:20:12 +0300

    Almost done with udemy course, create simple fps game

---
+++ Assets/Scripts/Mir/StartPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPos : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _pos;
+
+    public static StartPos GetInstance;
+
+    private void Awake()
+    {
+        GetInstance = this;
+    }
+
+    public Vector3 GetRandomPos()
+    {
+        return _pos[Random.Range(0, _pos.Count)].position;
+    }
+}
